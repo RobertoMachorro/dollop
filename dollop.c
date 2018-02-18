265636ce67f8ca3910cf0473c152e405b59e2e29
Roberto Machorro <RobertoMachorro@users.noreply.github.com>
2018-02-17 21:30:26 -0500

    Setup application parameter handling.

--- dollop.c
+++ dollop.c
@@ -1,5 +1,5 @@
 /*
-	Dollop v1.0 https://github.com/RobertoMachorro/dollop
+	Dollop - https://github.com/RobertoMachorro/dollop
 	Roberto Machorro. Contact author via GitHub.
 
 	Dollop is free software: you can redistribute it and/or modify
@@ -16,59 +16,57 @@
 	along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #include "argtable3.h"
 
-/* global arg_xxx structs */
-struct arg_lit *verb, *help, *version;
-struct arg_int *level;
-struct arg_file *o, *file;
+struct arg_lit *help, *version, *verbose, *binary;
+struct arg_int *size;
+struct arg_file *file;
 struct arg_end *end;
 
 int main(int argc, char *argv[])
 {
-    /* the global arg_xxx structs are initialised within the argtable */
-    void *argtable[] = {
-        help    = arg_litn(NULL, "help", 0, 1, "display this help and exit"),
-        version = arg_litn(NULL, "version", 0, 1, "display version info and exit"),
-        level   = arg_intn(NULL, "level", "<n>", 0, 1, "foo value"),
-        verb    = arg_litn("v", "verbose", 0, 1, "verbose output"),
-        o       = arg_filen("o", NULL, "myfile", 0, 1, "output file"),
-        file    = arg_filen(NULL, NULL, "<file>", 1, 100, "input files"),
-        end     = arg_end(20),
-    };
-    
-    int exitcode = 0;
-    char progname[] = "util.exe";
-    
-    int nerrors;
-    nerrors = arg_parse(argc,argv,argtable);
+	void *argtable[] = {
+		help = arg_litn(NULL, "help", 0, 1, "display this help and exit"),
+		version = arg_litn(NULL, "version", 0, 1, "display version info and exit"),
+		verbose = arg_litn(NULL, "verbose", 0, 1, "verbose output"),
+		binary = arg_litn("b", "binary", 0, 1, "write as binary (defaults to text)"),
+		size = arg_intn("s", "size", "<n>", 0, 1, "final file size in bytes, default 1k"),
+		file = arg_filen(NULL, NULL, "<file>", 0, 1, "output file, defaults to console"),
+		end = arg_end(20),
+	};
 
-    /* special case: '--help' takes precedence over error reporting */
-    if (help->count > 0)
-    {
-        printf("Usage: %s", progname);
-        arg_print_syntax(stdout, argtable, "\n");
-        printf("Demonstrate command-line parsing in argtable3.\n\n");
-        arg_print_glossary(stdout, argtable, "  %-25s %s\n");
-        exitcode = 0;
-    }
-    else if (version->count > 0)
+	int exitcode = 0;
+	char progname[] = "dollop";
+	char progdesc[] = "Creates files with pseudo-random content (dollop).";
+
+	int nerrors = arg_parse(argc,argv,argtable);
+
+	if (help->count > 0)
 	{
-	        printf("Version %d", 1);
-	        exitcode = 0;
+		printf("Usage: %s", progname);
+		arg_print_syntax(stdout, argtable, "\n");
+		printf("%s\n\n", progdesc);
+		arg_print_glossary(stdout, argtable, "  %-25s %s\n");
+		exitcode = 0;
 	}
-    /* If the parser returned any errors then display them and exit */
-    else if (nerrors > 0)
-    {
-        /* Display the error details contained in the arg_end struct.*/
-        arg_print_errors(stdout, end, progname);
-        printf("Try '%s --help' for more information.\n", progname);
-        exitcode = 1;
-    }
+	else if (version->count > 0)
+	{
+		printf("Dollop v%1.1f\n", 1.0);
+		printf("%s\n", progdesc);
+		printf("Copyright (c) 2018 Roberto Machorro\n");
+		exitcode = 0;
+	}
+	else if (nerrors > 0)
+	{
+		arg_print_errors(stdout, end, progname);
+		printf("Try '%s --help' for more information.\n", progname);
+		exitcode = 1;
+	}
+	else
+	{
+		printf("Work it\n\n");
+	}
 
-    /* deallocate each non-null entry in argtable[] */
-    arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
-    return exitcode;
+	arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+	return exitcode;
 }
-
