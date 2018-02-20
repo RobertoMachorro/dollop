c665d922a22f717601312eef09e86328a5bbc609
Roberto Machorro <RobertoMachorro@users.noreply.github.com>
2018-02-19 20:54:17 -0500

    Standardized help and version output to GNU. Cleaned up parameters, made GNU form.

--- main.c
+++ main.c
@@ -19,7 +19,7 @@
 #include "argtable3.h"
 #include "dollout.h"
 
-struct arg_lit *help, *version, *verbose;
+struct arg_lit *help, *version;
 struct arg_int *length;
 struct arg_file *file;
 struct arg_end *end;
@@ -27,33 +27,30 @@ struct arg_end *end;
 int main(int argc, char *argv[])
 {
 	void *argtable[] = {
-		help = arg_litn(NULL, "help", 0, 1, "display this help and exit"),
-		version = arg_litn(NULL, "version", 0, 1, "display version info and exit"),
-		verbose = arg_litn(NULL, "verbose", 0, 1, "verbose output"),
+		help = arg_litn("h", "help", 0, 1, "display this help and exit"),
+		version = arg_litn("V", "version", 0, 1, "display version info and exit"),
 		length = arg_intn("l", "length", "<n>", 0, 1, "final file length in bytes, default 1k"),
-		file = arg_filen(NULL, NULL, "<file>", 0, 1, "output file, defaults to console"),
+		file = arg_filen("o", "output", "<file>", 0, 1, "output file, defaults to console"),
 		end = arg_end(20),
 	};
 
 	int exitcode = 0;
 	char progname[] = "dollop";
-	char progdesc[] = "Creates files with pseudo-random content (dollop).";
 
 	int nerrors = arg_parse(argc,argv,argtable);
 
 	if (help->count > 0)
 	{
 		printf("Usage: %s", progname);
-		arg_print_syntax(stdout, argtable, "\n");
-		printf("%s\n\n", progdesc);
+		arg_print_syntax(stdout, argtable, "\n\n");
+		printf("Creates files with pseudo-random content (dollop). Generated content\nis sent to standard output, unless an output file is given.\n\n");
 		arg_print_glossary(stdout, argtable, "  %-25s %s\n");
+		printf("\nReport bugs to <https://github.com/RobertoMachorro/dollop/issues>.\nDollop home page: <https://github.com/RobertoMachorro/dollop>.\n");
 		exitcode = 0;
 	}
 	else if (version->count > 0)
 	{
-		printf("Dollop v%s\n", VERSION);
-		printf("%s\n", progdesc);
-		printf("Copyright (c) 2018 Roberto Machorro\n");
+		printf("Dollop %s\nCopyright (C) 2018 Roberto Machorro.\nLicense GPLv3+/Dollop: GNU GPL version 3 or later\n<http://gnu.org/licenses/gpl.html>, <http://gnu.org/licenses/exceptions.html>\nThis is free software: you are free to change and redistribute it.\nThere is NO WARRANTY, to the extent permitted by law.\n\nWritten by Roberto Machorro.\n", VERSION);
 		exitcode = 0;
 	}
 	else if (nerrors > 0)
